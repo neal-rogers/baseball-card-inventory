9940ba746072892961b7ade586e63f7deb26d2e6
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-02-18 19:45:32 -0500

    Removed header values from csv and json file.

--- collection.py
+++ collection.py
@@ -6,9 +6,8 @@
 import csv, json, graphx
 
 
-
 def csvimport(csvdata):
-    jsonfile = open('data\cards.json', 'w')
+    # jsonfile = open('data\cards.json', 'w')
     fields = (
             "UID",
             "PLAYER",
@@ -24,13 +23,17 @@ def csvimport(csvdata):
     for row in reader:
         json.dump(row, jsonfile)
         jsonfile.write('\n')
-
     jsonfile.close()
+
+def jsonread(jsondata):
+    jdata = json.loads(jsondata)
 
 def main():
     #print graphx.BANNER
     csvfile = open('data\cards.csv', 'r')
     csvimport(csvfile)
+    jsonfile = open('data\cards.json', 'w')
+    jsonread(jsonfile)
 
 if __name__ == '__main__':
     main()
